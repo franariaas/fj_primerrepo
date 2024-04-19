2de4aa3e025c08e4a0a038e78adc02e898cdbaf6
JAVIA <JAVIA@LAPTOP-8M4PP7G9>
2024-04-19 20:07:41 +0200

    añadido metodo toString

--- Empleado/src/empleado/Empleado.java
+++ Empleado/src/empleado/Empleado.java
@@ -2,4 +2,14 @@ package empleado;
 
 public class Empleado {
 	String nombre;
+
+	public Empleado(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [nombre=" + nombre + "]";
+	}
+	
 }
